07461aec4aab23c29d223f7d00b443d6e9655dbd
mitumial <ace.vortex76@gmail.com>
2024-11-18 19:08:29 -0500

    delete and generate entities

--- src/app/components/holiday-validator/holiday-validator.component.ts
+++ src/app/components/holiday-validator/holiday-validator.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-holiday-validator',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule],
   templateUrl: './holiday-validator.component.html',
   styleUrl: './holiday-validator.component.css'
 })

85a5e814ba928009af53a11ae02448c70cee58cb
mitumial <ace.vortex76@gmail.com>
2024-11-18 19:32:44 -0500

    add components to homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { HolidayValidatorComponent } from './components/holiday-validator/holiday-validator.component';
+import { TblHolidaysComponent } from './components/tbl-holidays/tbl-holidays.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, HolidayValidatorComponent, TblHolidaysComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/holiday-validator/holiday-validator.component.ts
+++ src/app/components/holiday-validator/holiday-validator.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-holiday-validator',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule,MatDividerModule,MatButtonModule],
   templateUrl: './holiday-validator.component.html',
   styleUrl: './holiday-validator.component.css'
 })
 export class HolidayValidatorComponent {
-
+  readonly startDate = new Date(2024, 0, 1);
 }
